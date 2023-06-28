961804e789027ff121527ed4fb7562fa8e226da4
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-06-28 21:21:03 +0200

    Solve 3rd challenge

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,29 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+export default function FormValidator() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
+  const [mssg, setMssg] = useState("");
+
+  const onErrors = () => {
+    const errors = [];
+    if (email.includes("@")) {
+      errors.push("Email Must contain @");
+    }
+    if (password.length < 8) {
+      errors.push("Password must be equal to 8");
+    }
+    return errors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const error = onErrors();
+
+    setMssg(error.length ? error.join(", ") : "User Created");
+  };
   return (
-    <form>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <h2>Sign Up!</h2>
-      <label htmlFor='email'>Email</label>
+      <label htmlFor="email">Email</label>
       <input
-        type='text' name='email'
-        onChange={e => setEmail(e.target.value)}
+        type="text"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <label htmlFor='password'>Password</label>
+      <label htmlFor="password">Password</label>
       <input
-        type='password' name='password'
-        onChange={e => setPassword(e.target.value)}
+        type="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <label htmlFor='password-confirm'>Confirm Password </label>
+      <label htmlFor="password-confirm">Confirm Password </label>
       <input
-        type='password' name='password-confirm'
-        onChange={e => setPasswordConfirm(e.target.value)}
+        type="password"
+        name="password-confirm"
+        onChange={(e) => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <h2>{mssg}</h2>
+      <input type="submit" value="Submit" />
     </form>
-  )
+  );
 }
