2bac2c9b9fd82ff56c721b4b7a9b8bc233935308
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-06-28 21:46:42 +0200

    Solve 4th challenge

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,26 @@
-export default function DogPics () {
-  // API: https://dog.ceo/dog-api/
+import axios from "axios";
+import { useState } from "react";
+
+export default function DogPics() {
+  const [img, setImage] = useState("");
+
+  const callDog = async () => {
+    try {
+      const response = await axios.get(
+        "https://dog.ceo/api/breeds/image/random"
+      );
+      console.log(response.data.message);
+
+      setImage(response.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      <img src={img} alt="Dog" />
+      <button onClick={callDog}>🐶</button>
     </div>
-  )
+  );
 }
